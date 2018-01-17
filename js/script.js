be9d2a974166d38d11a2d0181d56e858dd9a2be0
Eirik <eah001@student.uib.no>
2018-01-17 13:28:24 +0100

    la til flere markører.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 function initMap() {
     var heidis = {lat: 60.390106, lng: 5.320687};
     var fotballpuben = {lat: 60.390265, lng: 5.320959};
+    var lillebar = {lat: 60.391133, lng: 5.322045};
+    var vaskeriet = {lat: 60.391509, lng: 5.319852};
+    var bryggen = {lat:60.397495, lng: 5.322580};
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: heidis
@@ -12,6 +15,18 @@ function initMap() {
     });
     var marker2 = new google.maps.Marker({
       position: fotballpuben,
+      map: map
+    });
+    var marker3 = new google.maps.Marker({
+      position: lillebar,
+      map: map
+    });
+    var marker4 = new google.maps.Marker({
+      position: vaskeriet,
+      map: map
+    });
+    var marker5 = new google.maps.Marker({
+      position: bryggen,
       map: map
     });
   }
