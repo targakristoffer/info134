87c2e8d7da6c91dd2104787b34d16c6f6000cb80
Eirik <eah001@student.uib.no>
2018-04-11 13:43:19 +0200

    yalla

--- js/object.js
+++ js/object.js
@@ -57,11 +57,7 @@ var map = new google.maps.Map(document.getElementById('map'), {
 for(var i =0; i < jsonData.entries.length; i++){
   var teller = i + 1;
   var a =  teller.toString() + " " + jsonData.entries[i].plassering +" Adresse: " + jsonData.entries[i].adresse;
-<<<<<<< HEAD
-  var b = document.getElementById("toalettListe").innerHTML;
-=======
   var b = document.getElementById("Liste").innerHTML
->>>>>>> 8cf325ecdc840692a899ffa03398b39b8ac058ea
   var pos = {lat:parseFloat(jsonData.entries[i].latitude), lng:parseFloat(jsonData.entries[i].longitude)};
   document.getElementById("Liste").innerHTML = b + a + "<br>";
   var marker = new google.maps.Marker({
@@ -309,12 +305,12 @@ function velgFavorittToalett() {
     var x = document.createElement("SELECT");
     x.setAttribute("id", "mySelect");
     document.body.appendChild(x);
-    console.log(jsonData.entries[0].plassering);
-    for(var i = 0;i<jsonData.entries.length ;i++){
+
     var z = document.createElement("option");
-    z.setAttribute("value", "Toaletter");
-    var t = document.createTextNode(jsonData.entries[i].plassering);
+    z.setAttribute("value", "volvocar");
+    var t = document.createTextNode("a");
+    var s = document.createTextNode("aasdf");
     z.appendChild(t);
+    z.appendChild(s);
     document.getElementById("mySelect").appendChild(z);
-  }
 }
